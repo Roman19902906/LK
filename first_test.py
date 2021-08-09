12732c712a41bf5b90eedb57c602485c42fe7daf
Roman19902906 <85357325+Roman19902906@users.noreply.github.com>
2021-08-09 21:36:39 +0300

    Update first_test.py

--- first_test.py
+++ first_test.py
@@ -12,6 +12,8 @@ def start_fun():
 
 
 @pytest.yield_fixture(scope="class", autouse=True)
+
+
 def start_fun1():
     print("\nFirstTest class started")
     yield
@@ -21,8 +23,9 @@ def start_fun1():
 class TestFirst():
     def test_1(start_fun):
         print('Тест №1')
-        pytest.assume(2+2==4)
+        pytest.assume(2 + 2 == 4)
 
+        
     def test_2(start_fun):
         print('Тест №2')
         pytest.assume(2 + 2 == 5)
@@ -30,11 +33,10 @@ class TestFirst():
     def test_3(start_fun):
         print('Тест №3')
         pytest.assume(2 + 2 == 4)
-        pytest.assume(2 + 2 == 5)
 
     def test_4(start_fun):
         print('Тест №3')
-        pytest.assume(1/0 == 1)
+        pytest.assume(1 / 0 == 1)
 
     def test_5(start_fun):
         print('Тест №5')
