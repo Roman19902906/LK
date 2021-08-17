0498d3aaf12fca27ee3844ec5283b4e6c7b0052b
Roman19902906 <parkourR2012@yandex.ru>
2021-08-17 16:11:56 +0300

    Задание 9,10

--- conftest.py
+++ conftest.py
@@ -1,7 +1,14 @@
+import os
+import configparser
 import pytest
-import time
 from selenium import webdriver
 from login.pages.login_page import LoginPage
+import allure
+
+config = configparser.ConfigParser()
+config.read('settings.ini')
+config.get('User', "password")
+config.get('User', "login")
 
 
 @pytest.yield_fixture(scope="function", autouse=True)
@@ -20,7 +27,8 @@ def start_fun1():
 
 @pytest.fixture(scope="function")
 def browser():
-    link = 'https://tt-develop.quality-lab.ru/login'
+    #os.environ['URL'] = 'https://tt-develop1.quality-lab.ru'
+    link = os.getenv('URL', default='https://tt-develop.quality-lab.ru/login')
     browser = webdriver.Chrome()
     browser.get(link)
     print("\nstart browser..")
@@ -29,12 +37,22 @@ def browser():
     browser.quit()
 
 
+#import configparser
+
+#config = configparser.ConfigParser()
+#config.read('example.ini')
+#a = config.get('User', "password")
+
 @pytest.fixture(scope="function")
 def auth(browser):
-    input1 = browser.find_element(*LoginPage.USERNAME)
-    input1.send_keys("Авто Пользователь")
-    input2 = browser.find_element(*LoginPage.PASSWORD)
-    input2.send_keys("12345678")
-    button = browser.find_element(*LoginPage.BUTTON)
-    button.click()
-    yield browser
+    with allure.step("Авторизация"):
+        with allure.step("Логин Авто Пользователь"):
+            input1 = browser.find_element(*LoginPage.USERNAME)
+            input1.send_keys(config.get('User', "login"))
+        with allure.step("Пароль 12345678"):
+            input2 = browser.find_element(*LoginPage.PASSWORD)
+            input2.send_keys(config.get('User', "password"))
+        with allure.step("Вход"):
+            button = browser.find_element(*LoginPage.BUTTON)
+            button.click()
+        yield browser
